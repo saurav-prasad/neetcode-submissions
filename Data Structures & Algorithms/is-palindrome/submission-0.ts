f23812ba298220f6a8d8ee1e250acb0fe1b55cba
Saurav <six6x9yt@gmail.com>
2026-08-23 15:18:49 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+    let str = s.replace(/\s+/g, '');
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    const isOdd = (str.length % 2) != 0;
+
+    let pointerRight = str.length - 1;
+
+    let flag = true;
+
+    for (let i = 0; i < str.length; i++) {
+        if (isOdd && i+1 <= (str.length)/2) {
+            if (str[i] != str[pointerRight]) {
+                flag = false;
+                break
+            }
+        } else if(!isOdd && i+1 <= str.length/2) {
+              if (str[i] != str[pointerRight]) {
+                flag = false;
+                break
+            }
+        }
+        pointerRight = pointerRight - 1
+    }
+
+    return flag;
+    }
+}
